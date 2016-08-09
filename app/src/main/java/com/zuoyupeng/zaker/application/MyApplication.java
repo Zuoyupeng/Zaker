bec5bcb5abb02db038ffbb1a7775035c4cf15c94
Zuoyupeng <780707170@qq.com>
2016-08-09 19:57:18 +0800

    dhgdghdghfh

--- app/src/main/java/com/zuoyupeng/zaker/application/MyApplication.java
+++ app/src/main/java/com/zuoyupeng/zaker/application/MyApplication.java
@@ -17,8 +17,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        //iniiji
+        x.Ext.init(this);
+        x.Ext.setDebug(BuildConfig.DEBUG);
         OkHttpClient okHttpClient = new OkHttpClient.Builder()
                 .connectTimeout(10000L, java.util.concurrent.TimeUnit.MILLISECONDS)
                 .readTimeout(10000L, java.util.concurrent.TimeUnit.MILLISECONDS)
